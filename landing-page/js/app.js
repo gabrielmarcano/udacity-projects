b8cdf14e4457abcff6c019518ab17de800e83b5a
Gabriel <gabrielmarcano120@gmail.com>
2022-03-31 17:02:07 -0400

    Updated isActive func, created buildnav func

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -1,98 +1,93 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
- * 
-*/
+ *
+ */
 
-const sections = document.getElementsByTagName('section');
+var sections = document.getElementsByTagName("section");
 
-const fragment = document.createDocumentFragment();
+var fragment = document.createDocumentFragment();
 
-const navbar = document.getElementById('navbar__list');
+var navbar = document.getElementById("navbar__list");
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
-
-
+ *
+ */
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
 
-for (const section of sections) {
-    const listElement = document.createElement('li');
-
-    const anchorElement = document.createElement('a');
-    anchorElement.classList.add('menu__link');
-
-    anchorElement.innerHTML = `${section.attributes['data-nav'].value}`;
-    anchorElement.setAttribute('href', `#${section.id}`);
-
-    listElement.append(anchorElement);
-    fragment.appendChild(listElement);
+function buildNavItems() {
+    for (const section of sections) {
+        const listElement = document.createElement("li");
+    
+        const anchorElement = document.createElement("a");
+        anchorElement.classList.add("menu__link");
+    
+        anchorElement.innerHTML = `${section.attributes["data-nav"].value}`;
+        anchorElement.setAttribute("href", `#${section.id}`);
+    
+        listElement.append(anchorElement);
+        fragment.appendChild(listElement);
+    }
+    
+    navbar.appendChild(fragment);
 }
-
-navbar.appendChild(fragment);
 
 
 // Add class 'active' to section when near top of viewport
 
 function isActive() {
     for (const section of sections) {
-      const box = section.getBoundingClientRect();
-      if (box.top <= 100 && box.bottom >= 100) {
-        console.log(`${section.id} is active. Added class active`);
-      } else {
-        console.log(`${section.id} is not active. Removed class active`);
-      }
+        const sectionBoundary = section.getBoundingClientRect();
+        if (sectionBoundary.top <= 100 && sectionBoundary.bottom >= 100) {
+            section.classList.add('active');
+            document.querySelector(`.menu__link[href="#${section.id}"]`).classList.add('active-link');
+        } else {
+            section.classList.remove('active');
+            document.querySelector(`.menu__link[href="#${section.id}"]`).classList.remove('active-link');
+        }
     }
-  }
+}
 
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
 
-
-
-
-
-
-
-
+document.addEventListener('scroll', isActive);
