c0c23d51fb9fe6437669660b620db8e89683e029
Gabriel <gabrielmarcano120@gmail.com>
2022-04-08 17:28:04 -0400

    Added API key

--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -1,3 +1,6 @@
+// Personal API Key for OpenWeatherMap API
+const apiKey = 'f029632a333bf5bdf8b4795dd060bc0f&units=imperial';
+
 /* Global Variables */
 
 // Create a new date instance dynamically with JS
