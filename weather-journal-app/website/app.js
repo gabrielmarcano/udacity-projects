e38bfe8c10273ec3953c7d2b2f33e652404db70a
Gabriel <gabrielmarcano120@gmail.com>
2022-04-08 18:05:20 -0400

    Client side get data function

--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -6,3 +6,16 @@ const apiKey = 'f029632a333bf5bdf8b4795dd060bc0f&units=imperial';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+// Asynchronously fetch the data from the app endpoint
+async function getData(url='', key='') {
+    const response = await fetch(url+key);
+    try {
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.log("Error: ", error);
+    }
+}
