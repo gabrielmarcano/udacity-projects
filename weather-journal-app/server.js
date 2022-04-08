a60a54389f701d8dba000c15ad7f0f25d3945910
Gabriel <gabrielmarcano120@gmail.com>
2022-04-08 16:30:10 -0400

    Added body-parser require

--- weather-journal-app/server.js
+++ weather-journal-app/server.js
@@ -9,6 +9,7 @@ const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
